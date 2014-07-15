d060c9e464b6efddb826cb77113ceb073be1053f
anton <anton@1ffdbadb-e8b5-4aae-beac-db3f2f44b0cf>
2014-07-15 11:26:00 +0000

    [277] Also fixed allowing a recursive mock to work with interfaces
    git-svn-id: svn+ssh://svn.earth.pensio.com/home/subversion/tools/trunk@605 1ffdbadb-e8b5-4aae-beac-db3f2f44b0cf

--- lib/mockit/MockHelperGenerator.class.php
+++ lib/mockit/MockHelperGenerator.class.php
@@ -76,7 +76,7 @@ class MockHelperGenerator
 				$parameters[] = $paramString;
 			}
 
-			if(preg_match('/\@return\s+(\S+)/',$method->getDocComment(), $matches) && !in_array($matches[1], array('void','mixed','string')) && class_exists($matches[1]))
+			if(preg_match('/\@return\s+(\S+)/',$method->getDocComment(), $matches) && !in_array($matches[1], array('void','mixed','string')) && (class_exists($matches[1]) || interface_exists($matches[1])))
 			{
 				$withImplementationCode .= "\t/**\n\t *  @return Mock_".$matches[1]."\n\t */\n\tfunction ".$method->getName()."(".implode(',',$parameters).");\n\n";
 
