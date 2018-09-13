2222bcb38b0a845f9784e9c76a45684c962f974a
roman <roman@1ffdbadb-e8b5-4aae-beac-db3f2f44b0cf>
2018-09-13 10:12:42 +0000

    [s19] Require autoloader once to be able to run tests in a suite
    git-svn-id: svn+ssh://svn.earth.pensio.com/home/subversion/tools/trunk@1061 1ffdbadb-e8b5-4aae-beac-db3f2f44b0cf

--- test/MockFailingTests.php
+++ test/MockFailingTests.php
@@ -1,5 +1,5 @@
 <?php
-require dirname(__FILE__).'/../autoload.php';
+require_once dirname(__FILE__).'/../autoload.php';
 
 class MockFailingTests
 	extends MockitTestCase
--- test/MockTest.php
+++ test/MockTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require dirname(__FILE__).'/../autoload.php';
+require_once dirname(__FILE__).'/../autoload.php';
 
 class MockTest
 	extends MockitTestCase
